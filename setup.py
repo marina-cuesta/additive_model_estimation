44522d696fcb1aff1c2cb3c539e50bfd932d96b9
marina-cuesta <marinacuesta.st@gmail.com>
2024-11-12 17:38:12 +0100

    project initialization

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mi_primer_proyecto",
+    version="1.0",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author="Marina la mejor",
+    author_email="",
+)
